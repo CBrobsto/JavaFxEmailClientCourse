a3d23e976f6b01431866ab41c218a90d9ae431cd
Brobston (CTR) <cbrobston@progeny.net>
2021-03-15 10:39:22 -0400

    Added multithreading to java mail.

--- src/com/cbrobsto/controller/LoginWindowController.java
+++ src/com/cbrobsto/controller/LoginWindowController.java
@@ -31,21 +31,19 @@ public class LoginWindowController extends BaseController {
         if(fieldsAreValid()) {
             EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
             LoginService loginService = new LoginService(emailAccount, emailManager);
-            EmailLoginResult emailLoginResult = loginService.login();
-
-            switch (emailLoginResult) {
-                case SUCCESS:
-                    System.out.println("login succesfull!!!" + emailAccount);
-                    viewFactory.showMainWindow();
-                    Stage stage = (Stage) errorLabel.getScene().getWindow();
-                    viewFactory.closeStage(stage);
-                    return;
-            }
+            loginService.start();
+            loginService.setOnSucceeded(event -> {
+                EmailLoginResult emailLoginResult= loginService.getValue();
+                switch (emailLoginResult) {
+                    case SUCCESS:
+                        System.out.println("login succesfull!!!" + emailAccount);
+                        viewFactory.showMainWindow();
+                        Stage stage = (Stage) errorLabel.getScene().getWindow();
+                        viewFactory.closeStage(stage);
+                        return;
+                }
+            });
         }
-
-//        viewFactory.showMainWindow();
-//        Stage stage = (Stage) errorLabel.getScene().getWindow();
-//        viewFactory.closeStage(stage);
     }
 
     private boolean fieldsAreValid() {
